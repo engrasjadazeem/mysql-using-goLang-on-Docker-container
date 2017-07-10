1d305b7c7fac6071217ad98d9fe84faa818b3462
Asjad Azeem <asjad.azeem@tenpearls.com>
2017-07-10 17:16:47 +0500

    First commit

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+#Read https://severalnines.com/blog/mysql-docker-containers-understanding-basics
+########################################################################################################################
+#Running an instance of mysql on terminal
+#$ docker run --detach --name=test-mysql --env="MYSQL_ROOT_PASSWORD=<mypassword>" mysql
+#To see logs of container
+#$ docker logs test-mysql
+#Install sql client
+#$ apt-get install mysql-client
+#Get your IP Address
+#$ docker inspect test-mysql
+#Connect to mysql
+#$ mysql -uroot -p<mypassword> -h <IPAdress(172.17.0.02)> -P 3306
+
+FROM golang:1.8.3
+LABEL maintainer "Asjad"
+
+RUN apt-get update && apt-get install -y mysql-client
+EXPOSE 8080
+
+
+ENV mySQLIPAddress 172.17.0.2
+ENV mySQLIPPort 3306
+ENV mySQLPassword mypassword
+
+RUN echo $mySQLIPAddress
+RUN echo $mySQLIPPort
+RUN echo $mySQLPassword
+
+#Run new container linking to mysql
+#docker run -dit --name test-link --link test-mysql:mysql linkcontainers
+
+#Run Testdb
+#mysql -uroot -p$mySQLPassword -h $mySQLIPAddress -P $mySQLIPPort --database=testdb
+
+#Create Table in mysql
+
+
+
+#########################
+RUN go get github.com/go-sql-driver/mysql
+RUN mkdir /app
+ADD . /app/
+WORKDIR /app
+RUN go build -o main .
+CMD ["/app/main"]
+#########################
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"os"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+type person struct {
+	Id int
+	LastName string
+	FirstName string
+	Address string
+	City string
+}
+
+func main() {
+
+	fmt.Println("Opening connection")
+	db, err := sql.Open("mysql", "root:"+os.Getenv("mySQLPassword")+"@tcp("+os.Getenv("mySQLIPAddress")+":"+ os.Getenv("mySQLIPPort") +")/testdb")
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	fmt.Println("Connection opened successfully!")
+
+	// Last thing to do, close connection
+	defer db.Close()
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	// Execute the query
+	var (
+		Id int
+		LastName string
+		FirstName string
+		Address string
+		City string
+	)
+
+	//// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	fmt.Println(columns)
+
+	///////////// Read Operation ///////////////////
+	rowsQuery, err := db.Prepare("SELECT * FROM Persons where PersonID = ?")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer rowsQuery.Close()
+	// Parametrize
+	rows, err := rowsQuery.Query(1)
+	for rows.Next() {
+		err := rows.Scan(&Id,  &LastName, &FirstName, &Address, &City)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(Id, LastName, FirstName, Address, City)
+	}
+
+	//////////// Insertion ////////////////////////
+	//stmt, err := db.Prepare("INSERT INTO Persons(PersonID, LastName, FirstName, Address, City) VALUES(?,?,?,?,?)")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	////Single Record Addition
+	//res, err := stmt.Exec("2", "Aslam", "Atif", "ABC", "Lahore")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	// For MultiInsertion Case struct can be use that can be dumped with data from any source like: var persons = []person{}
+
+	//META
+	//lastId, err := res.LastInsertId()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//rowCnt, err := res.RowsAffected()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+
+	///////////// Delete a Record ////////////////
+	//_, err = db.Exec("DELETE FROM Persons where PersonID = ?", 2)  // OK
+	//if err!=nil{
+	//	log.Fatal(err)
+	//}
+
+	///////////// Update a Record ////////////////
+	//_, err = db.Exec("UPDATE Persons SET City = 'Karachi' where PersonID = ?", 2)  // OK
+	//if err!=nil{
+	//	log.Fatal(err)
+	//}
+
+}
